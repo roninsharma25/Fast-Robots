948c2afaa88d70bafd28946c34b83e6a4cc5e2f1
Ronin Sharma <rrs234@cornell.edu>
2022-05-04 16:34:20 -0400

    lab 12 updates

--- Lab12/cmd_types.py
+++ Lab12/cmd_types.py
@@ -2,13 +2,8 @@ from enum import Enum
 
 class CMD(Enum):
     PING = 0
-    SEND_TWO_INTS = 1
-    SEND_THREE_FLOATS = 2
-    ECHO = 3
-    DANCE = 4
-    SET_VEL = 5
-    MOVE_FORWARD = 6
-    STOP_ROBOT = 7
-    GET_IMU = 8
-    UPDATE_PID = 9
-    TURN = 10
+    MOVE_FORWARD = 1
+    STOP_ROBOT = 2
+    UPDATE_PID = 3
+    TURN = 4
+    TURN_360 = 5
